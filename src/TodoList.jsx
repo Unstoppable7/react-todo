99b97f9fd022677c2d4f2845b54d1eaaacae9c4e
Cristian Rosales <alejandro7120@gmail.com>
2024-09-11 03:47:47 -0400

    answers for lesson_3_1

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,10 @@
 import TodoListItem from "./TodoListItem";
+import styles from './TodoList.module.css'
 
 export default function TodoList({ todoList, onRemoveTodo}) {
 
   return (
-    <ul>
+    <ul className={styles.content}>
       {
         todoList.map(
           (item) => <TodoListItem key={item.id} todo={item} onRemoveTodo={onRemoveTodo} />
